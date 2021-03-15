c3ec8533cd8d7d5b3555ffac885cf7597f80123a
Saidramadhan18 <said.romadhan18@gmail.com>
2021-03-15 11:27:24 +0700

    Hari pertama ngoding offline

---
+++ Youtube Clone/Video.swift
@@ -0,0 +1,19 @@
+//
+//  Video.swift
+//  Youtube Clone
+//
+//  Created by Muhammad Said Ramadhan on 15/03/21.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+    
+    
+}
